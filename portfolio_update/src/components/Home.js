6ed9e50ed155306bde2d7092de01386775f3ecb4
Wesley <wspencer3295@yahoo.com>
2021-05-19 16:10:49 -0700

    deploy

--- portfolio_update/src/components/Home.js
+++ portfolio_update/src/components/Home.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Home.css'
-
+import Resume from './Resume'
 const Home = () => {
+    const [resumeDisplay, setResumeDisplay] = useState("none")
+    const renderResume = () => {
+        setResumeDisplay("inline")
+    }
+    console.log(resumeDisplay)
     return (
         <>
             <div id="portfolio__container" className="portfolio__container">
@@ -13,12 +18,12 @@ const Home = () => {
                     <a href="https://www.linkedin.com/in/web-dev-spencer/"><i className="fab fa-linkedin"></i></a>
                     <a href="https://wesley3295.medium.com/"><i className="fab fa-medium-m"></i></a>
                     <a href="wesley3295@gmail.com"><i className="far fa-envelope"></i></a>
-                    <i className="far fa-file portfolio__resumeIcon"></i>
+                    <i onClick={renderResume} className="far fa-file portfolio__resumeIcon"></i>
                 </div>
                 <p>A Full-Stack Software Engineer with experience in developing React, Redux, and Ruby-on-Rails web applications. Adaptable and driven with a strong work ethic, always looking to build and grow scalable applications to solve real-world problems. I bring strong skills in research, debugging, and communication to the team with experience in leadership and the desire to help others. Ability to thrive in a flexible team environment, demonstrate critical decision-making skills, and have strong interpersonal and communication abilities. All and more of which stem from my 10 years experience working in the Hospitality Industry.  </p>
             </div>
+                    <Resume resumeDisplay={resumeDisplay} setResumeDisplay={setResumeDisplay} />
            
-
         </>
     )
 }
--- portfolio_update/src/components/Resume.js
+++ portfolio_update/src/components/Resume.js
@@ -1,19 +1,28 @@
-import React from "react";
 import "./Resume.css";
-const Resume = () => {
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+const Resume = ({resumeDisplay, setResumeDisplay}) => {
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+  const exitResume=(e)=>{
+    setResumeDisplay("none")
+    e.target.parentNode.parentNode.style.display = resumeDisplay
+  }
   return (
-    <div>
-      <div
-        className="portfolio__resume"
-        id="portfolio__resume"
-        style={{ display: "none" }}
+    <div className="portfolio__resume" style={{display:resumeDisplay}}>
+      <div className="resume__exit"><i onClick={(e)=>exitResume(e)}id="resume__exit" className="far fa-times-circle"></i></div>
+      
+       <Document 
+        file="resume.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
       >
-        <i id="resume__exit" className="far fa-times-circle resume__exit"></i>
-
-        {/* <object data="resume.pdf" type="application/pdf" width="100%" height="100%">
-    <a href="resume.pdf"></a>
-</object> */}
-      </div>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>Page {pageNumber} of {numPages}</p>
     </div>
   );
 };
