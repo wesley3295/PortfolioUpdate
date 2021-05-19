bdbdebc333af0d5e9c34979693bea9fe371fb614
Wesley <wspencer3295@yahoo.com>
2021-05-19 16:18:52 -0700

    update

--- portfolio_update/src/App.js
+++ portfolio_update/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
-import ParticlesContainer from "./components/ParticlesContainer";
 import Particles from 'react-particles-js';
 import "./components/ParticlesContainer.css"
 
