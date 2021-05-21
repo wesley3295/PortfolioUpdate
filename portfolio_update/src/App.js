d2f900dae960703b84cca8bceb7004c48efceb54
Wesley <wspencer3295@yahoo.com>
2021-05-21 15:38:14 -0700

    responsive ui

--- portfolio_update/src/App.js
+++ portfolio_update/src/App.js
@@ -5,136 +5,136 @@ import RecipeReviewCard from "./components/Home";
 import Particles from 'react-particles-js';
 import "./components/ParticlesContainer.css"
 import COLOR_IMAGE1 from './colorMe1.jpg'
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
     <Router>
-      <Particles 
-      ClassName = "particles-js"
-      id="particles-js"
-    params={{
-      particles: {
-        number: {
-          value: 400,
-          density: {
-            enable: true,
-            value_area: 2000
-          }
-        },
-        color: {
-          value: "#5adbd5"
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#5adbd5"
-          },
-          polygon: {
-            nb_sides: 5
-          },
-          image: {
-            src: "",
-            width: 100,
-            height: 100
-          }
-        },
-        opacity: {
-          value: 0.5,
-          random: false,
-          anim: {
-            enable: false,
-            speed: 1,
-            opacity_min: 0.1,
-            sync: false
-          }
-        },
-        size: {
-          value: 5,
-          random: true,
-          anim: {
-            enable: false,
-            speed: 40,
-            size_min: 0.1,
-            sync: false
-          }
-        },
-        line_linked: {
-          enable: true,
-          distance: 150,
-          color: "#898a7c",
-          opacity: 0.4,
-          width: 1
-        },
-        move: {
-          enable: true,
-          speed: 1,
-          direction: "none",
-          random: false,
-          straight: false,
-          out_mode: "out",
-          attract: {
-            enable: false,
-            rotateX: 600,
-            rotateY: 1200
-          }
-        }
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: true,
-            mode: "bubble"
-          },
-          onclick: {
-            enable: true,
-            mode: "grab"
-          },
-          resize: true
-        },
-        modes: {
-          grab: {
-            distance: 400,
-            line_linked: {
-              opacity: 1
-            }
-          },
-          bubble: {
-            distance: 100,
-            size: 30,
-            duration: 2,
-            opacity: 4,
-            speed: 1
-          },
-          repulse: {
-            distance: 50
-          },
-          push: {
-            particles_nb: 4
-          },
-          remove: {
-            particles_nb: 2
-          }
-        }
-      },
-      retina_detect: true,
-      config_demo: {
-        hide_card: false,
-        background_color: "#b61924",
-        background_image: "",
-        background_position: "50% 50%",
-        background_repeat: "no-repeat",
-        background_size: "cover"
-      }
-    }}
-      >
-    </Particles>
-    <Route exact path="/"><RecipeReviewCard/></Route>
+    <Route exact path="/"><RecipeReviewCard /></Route>
+          <Particles
+            // ClassName="particles-js"
+            // id="particles-js"
+            params={{
+              particles: {
+                number: {
+                  value: 300,
+                  density: {
+                    enable: true,
+                    value_area: 2000
+                  }
+                },
+                color: {
+                  value: "#5adbd5"
+                },
+                shape: {
+                  type: "circle",
+                  stroke: {
+                    width: 0,
+                    color: "#5adbd5"
+                  },
+                  polygon: {
+                    nb_sides: 5
+                  },
+                  image: {
+                    src: "",
+                    width: 100,
+                    height: 100
+                  }
+                },
+                opacity: {
+                  value: 0.5,
+                  random: false,
+                  anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false
+                  }
+                },
+                size: {
+                  value: 5,
+                  random: true,
+                  anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false
+                  }
+                },
+                line_linked: {
+                  enable: true,
+                  distance: 150,
+                  color: "#898a7c",
+                  opacity: 0.4,
+                  width: 1
+                },
+                move: {
+                  enable: true,
+                  speed: 1,
+                  direction: "none",
+                  random: false,
+                  straight: false,
+                  out_mode: "out",
+                  attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                  }
+                }
+              },
+              interactivity: {
+                detect_on: "canvas",
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: "bubble"
+                  },
+                  onclick: {
+                    enable: true,
+                    mode: "grab"
+                  },
+                  resize: true
+                },
+                modes: {
+                  grab: {
+                    distance: 400,
+                    line_linked: {
+                      opacity: 1
+                    }
+                  },
+                  bubble: {
+                    distance: 100,
+                    size: 30,
+                    duration: 2,
+                    opacity: 4,
+                    speed: 1
+                  },
+                  repulse: {
+                    distance: 50
+                  },
+                  push: {
+                    particles_nb: 4
+                  },
+                  remove: {
+                    particles_nb: 2
+                  }
+                }
+              },
+              retina_detect: true,
+              config_demo: {
+                hide_card: true,
+                background_color: "#b61924",
+                background_image: "",
+                background_position: "100% 100%",
+                background_repeat: "no-repeat",
+                background_size: "cover"
+              }
+            }}
+          >
 
-        {/* <Route exact path="/"><Home/></Route> */}
+          </Particles>
+          
         
-      
     </Router>
   );
 }
--- portfolio_update/src/components/Home.js
+++ portfolio_update/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 // import './Home.css'
 // import Resume from './Resume'
 // const Home = () => {
@@ -23,123 +23,145 @@ import React, {useState} from 'react'
 //                 <p>A Full-Stack Software Engineer with experience in developing React, Redux, and Ruby-on-Rails web applications. Adaptable and driven with a strong work ethic, always looking to build and grow scalable applications to solve real-world problems. I bring strong skills in research, debugging, and communication to the team with experience in leadership and the desire to help others. Ability to thrive in a flexible team environment, demonstrate critical decision-making skills, and have strong interpersonal and communication abilities. All and more of which stem from my 10 years experience working in the Hospitality Industry.  </p>
 //             </div>
 //                     <Resume resumeDisplay={resumeDisplay} setResumeDisplay={setResumeDisplay} />
-           
+
 //         </>
 //     )
 // }
 
 // export default Home
 
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import PictureAsPdfOutlinedIcon from '@material-ui/icons/PictureAsPdfOutlined';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import Grid from '@material-ui/core/Grid';
-import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import PictureAsPdfOutlinedIcon from "@material-ui/icons/PictureAsPdfOutlined";
+import Grid from "@material-ui/core/Grid";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/core/styles";
 
-import GRAY_IMAGE1 from '../grayMe1.jpg'
-import COLOR_IMAGE1 from '../colorMe1.jpg'
-
-const y = 50;
-const x = 50;
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 450,
-    position: 'absolute',
-    left: `${43}%`,
-    top: `${0}%`,
-    transform: `translate(${x}px, ${y}px)`,
-    backgroundColor:"rgba(0,0,0,0.8)",
-    
-},
-media: {
-    height: 423,
-    paddingTop: '56.25%', // 16:9,
-    filter: "grayscale(100%)",
-    transitionDuration:"1000ms",
-    transitionProperty: "backgroundImage",
-
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-  spacing:{
-      paddingLeft:"30px",
-      
-  }
-}));
+import COLOR_IMAGE1 from "../colorMe1.jpg";
 
 export default function RecipeReviewCard() {
+  const y = 50;
+  const x = 50;
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("xs"));
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 350,
+      [theme.breakpoints.down("xs")]: {
+        marginRight: 100,
+      },
+      marginRight: 0,
+      position: "absolute",
+      top: `${10}%`,
+      transform: `translate(${x}px, ${y}px)`,
+      backgroundColor: "rgba(0,0,0,0.8)",
+      overflow: "hidden",
+    },
+    media: {
+      overflow: "hidden",
+      height: 400,
+      paddingTop: 0,
+    },
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: "rotate(180deg)",
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+    spacing: {
+      paddingRight: "10px",
+    },
+  }));
   const classes = useStyles();
 
-const colorPhoto = (e) =>{
-    e.target.style.filter = "grayscale(0%)"
-}
-const grayPhoto=(e)=>{
-    e.target.style.transitionDelay = "2s"
-    // e.target.style.transitionDuration = "1000ms"
-    // e.target.style.transitionProperty = "backgroundImage"
-    e.target.style.filter = "grayscale(100%)"
-}
+  const colorPhoto = (e) => {
+    console.log(e.target);
+    e.target.style.filter = "grayscale(0%)";
+  };
+  const grayPhoto = (e) => {
+    e.target.style.filter = "grayscale(100%)";
+  };
   return (
-  
-    <Card className={classes.root} >
-     
-      <CardMedia
-        className={classes.media}
-        image={COLOR_IMAGE1}
-        title="Picture"
-        onMouseEnter={(e) => colorPhoto(e)}
-        onMouseLeave={(e) => grayPhoto(e)}
-      />
-      <CardContent>
-        <Typography variant="body2" color="colorTextPrimary" component="p" style={{color:"white"}}>
-        A Full-Stack Software Engineer with experience in developing React, Redux, and Ruby-on-Rails web applications. Adaptable and driven with a strong work ethic, always looking to build and grow scalable applications to solve real-world problems. I bring strong skills in research, debugging, and communication to the team with experience in leadership and the desire to help others. Ability to thrive in a flexible team environment, demonstrate critical decision-making skills, and have strong interpersonal and communication abilities. All and more of which stem from my 10 years experience working in the Hospitality Industry.
-        </Typography>
-      </CardContent>
-      <CardActions className={classes.spacing}  >
-        <IconButton aria-label="Github"  style={{color:"white"}}>
-          <GitHubIcon style={{ fontSize:"48px"}}/>
-        </IconButton>
-        <IconButton aria-label="LinkedIn"  style={{color:"white"}}>
-          <LinkedInIcon style={{ fontSize:"48px"}} />
-        </IconButton>
-        <IconButton aria-label="Medium"  style={{color:"white"}}>
-        <i className="fab fa-medium-m" style={{ fontSize:"48px"}}></i>
-        </IconButton>
-        <IconButton aria-label="E-Mail"  style={{color:"white"}}>
-          <MailOutlineIcon style={{ fontSize:"48px"}}/>
-        </IconButton>
-        <IconButton aria-label="Resume.pdf"  style={{color:"white"}}>
-          <PictureAsPdfOutlinedIcon style={{ fontSize:"48px"}}/>
-        </IconButton>
-        
-      </CardActions>
-      
-    </Card>
+    <Grid container justify="center" alignItems="center">
+      <Card className={classes.root}>
+        <Grid item>
+          <CardMedia className={classes.media} title="Picture">
+            <img
+              src={COLOR_IMAGE1}
+              alt=""
+              height="400"
+              width="350"
+              style={{ filter: "grayscale(100%)" }}
+              onMouseEnter={(e) => colorPhoto(e)}
+              onMouseLeave={(e) => grayPhoto(e)}
+            />
+          </CardMedia>
+        </Grid>
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="colorTextPrimary"
+            component="p"
+            style={{ color: "white" }}
+          >
+            A Full-Stack Software Engineer with experience in developing React,
+            Redux, and Ruby-on-Rails web applications. Adaptable and driven with
+            a strong work ethic, always looking to build and grow scalable
+            applications to solve real-world problems. I bring strong skills in
+            research, debugging, and communication to the team with experience
+            in leadership and the desire to help others. Ability to thrive in a
+            flexible team environment, demonstrate critical decision-making
+            skills, and have strong interpersonal and communication abilities.
+            All and more of which stem from my 10 years experience working in
+            the Hospitality Industry.
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.spacing}>
+          <IconButton aria-label="Github" >
+            <a href="https://github.com/wesley3295">
+              <GitHubIcon style={{ fontSize: "40px",color: "white" }} />
+            </a>
+          </IconButton>
+          <IconButton aria-label="LinkedIn" >
+            <a
+              href="https://www.linkedin.com/in/web-dev-spencer/"
+            >
+              <LinkedInIcon style={{ fontSize: "40px",color: "white" }} />
+            </a>
+          </IconButton>
+          <IconButton aria-label="Medium" >
+            <a
+              href="https://wesley3295.medium.com/"
+            >
+              <i className="fab fa-medium-m" style={{ fontSize: "40px",color: "white" }}></i>
+            </a>
+          </IconButton>
+          <IconButton aria-label="E-Mail" >
+            <a href="mailto:wesley3295@gmail.com">
+              <MailOutlineIcon style={{ fontSize: "40px",color: "white" }} />
+            </a>
+          </IconButton>
+          <IconButton aria-label="Resume.pdf" >
+            <PictureAsPdfOutlinedIcon style={{ fontSize: "40px",color: "white" }} />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 }
